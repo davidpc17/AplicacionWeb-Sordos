bde98bdd39fe9988ca6ff699f5ee1366ff99a080
romariopater94 <rmirandap@uniguajira.edu.co>
2020-07-11 16:06:37 -0500

    Romariopater94

--- back_end/ingresecode.php
+++ back_end/ingresecode.php
@@ -1,5 +1,5 @@
 <?php
-include('../controller/load.php');
+require('includes/load.php');
 if(isset($_SESSION["email"]) && isset(($_SESSION["clave"] ))){
 
 	$u = $_SESSION["email"];
@@ -14,57 +14,57 @@ if(isset($_SESSION["email"]) && isset(($_SESSION["clave"] ))){
 header('location:portal.php');
 
 }
-	}else{$obligatorio = 1;
+}else{
+//----------------------------------------------------------------------------
+	
+$obligatorio = 1;
 if ($obligatorio) $requerido = "required";
 else $requerido = "";
-require_once 'PHPMailer/PHPMailerAutoload.php';
-	if(isset($_POST['enviar']))
-{	
-		$mail = new PHPMailer();
-		$mail->isSMTP();
-		$mail->SMTPAuth = true;
-		$mail->SMTPDebug = 0;
-		$mail->Debugoutput = 'html';
-		$mail->SMTPSecure = 'tls';
-		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = '587';
-		
-		$mail->Username = 'electivaprogweb.luis@gmail.com';
-		$mail->Password = '19pruebaslp2019';  
-   $mail->setFrom('electivaprogweb.luis@gmail.com', 'No-reply');
-
-    $u = $_POST['email'];
-     $sql = "SELECT email FROM usuario WHERE email ='$u'";
+//----------------------------------------------------------------------------
+if(isset($_POST['enviar']))
+{
+$cadena_de_texto = $_POST['email'];
+$cadena_buscada ="@";	
+$posicion_coincidencia = strrpos($cadena_de_texto, $cadena_buscada);
+if($posicion_coincidencia === false){
+	$mensaje = "<font color='#FF0000'>Se es necesario un correo electronico lo que ha ingresado  ". $_POST['email'] . "   No es un correo electronico </font>";
+}
+$encriptar =  SED::encryption($_POST['clave']);
+$code = $_POST['code'];
+$u = $_POST['email'];
+$encriptar =  SED::encryption($_POST['clave']);
+ $sql = "SELECT email FROM user WHERE email ='$u'";
   list($a,$asociar,$e)=select($sql);
 
   $correo= $asociar['email'];
   if ($u==$correo) {
-      
-    $mail->addAddress($u,$u); 
-       
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Recuperacion de contraseña';
-    $codigo_rand = rand(100000,999999);
-    $sql = "UPDATE `user` SET `code_uptdate`='$codigo_rand' WHERE email = '$u'";
-  ejecutar($sql);
-    $mail->Body    = 'Su codigo de recuperacion es'. ' '. $codigo_rand. ' '. 'de click aqui para ir al link donde ingresara este codigo'.' '."http://52.170.185.100/parcial/ingresecode.php";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    if(!$mail->send()) {
-    	echo 'El mensaje no pudo ser enviado.';
-    	echo 'Mailer Error: ' . $mail->ErrorInfo;
-    	$enviado = "<font color='#FF0000'> error este correo no existe ".$u." "."corrija y vuelva a intentar";
-	} else {
-    	echo 'Mensaje ha sido enviado';
-    	$enviado = "<font color='#0BAF38'> Su codigo ha sido enviado con exito al sigt. email ".$u. "de click en tengo mi codigo";
+$sql2= "UPDATE `user` SET `clave`= '$encriptar'  WHERE code_uptdate ='$code' AND email = '$u'";
+$a=ejecutar($sql2);
+$sql3= "UPDATE `user` SET `code_uptdate`= NULL WHERE code_uptdate ='$code' AND email = '$u'";
+	ejecutar($sql3);
+$p = $_POST['clave'];
+    $sql = "SELECT clave FROM user WHERE email ='$u'";
+  list($a,$asociar,$e)=select($sql);
+
+  $pass= $asociar['clave'];
+$desencriptar = SED::decryption($pass); // METODO DE DESENCRiptado
+$uppercase =preg_match('@[A-Z]@', $p); $lowercase = preg_match('@[a-z]@', $p); $number = preg_match('@[0-9]@', $p); if(!$uppercase || !$lowercase || !$number || strlen($p) < 8) { $mensaje ="<font color='#FF0000'> Su clave debe contar con lo minimo con letras minusculas, mayuscula y numeros y tener mas de 8 caracteres"; }
+else{
+if ($p==$desencriptar) {
+
+
+$mensaje = "<font color='#0BAF38'> Su credencial a sido restaurada";
 }
-
+else{
+$mensaje = "<font color='#FF0000'> Los datos ingresados son errados porfavor verificar y volver a intentar";
 }
-
+}else{
+	$mensaje =  "<font color='#FF0000'> Su correo es invalido intente nuevanmente";
 }
-
-
-
+}
+}
+}
 ?>
 
 <!DOCTYPE html>
@@ -96,13 +96,13 @@ require_once 'PHPMailer/PHPMailerAutoload.php';
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-02.png" alt="IMG">
+					<img src="images/img-04.png" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form"  class="form" action="" method="post">
         
 					<span class="login100-form-title">
-						Olvide mi Contraseña 
+						Member Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
@@ -112,32 +112,43 @@ require_once 'PHPMailer/PHPMailerAutoload.php';
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</span>
 					</div>
-
-					
+          <div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="number" name="code" placeholder="Codigo" <?php echo $requerido ?>>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="clave" placeholder="Password" <?php echo $requerido ?>>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
 					
 					<div class="container-login100-form-btn">
-						<input type="submit" value="Obtener codigo" name="enviar" class="login100-form-btn" >
-							
-						
-					</div>
-<?php
-if(isset($_POST['enviar']))
+						<input type="submit" value="Recuperar" name="enviar" class="login100-form-btn" >
+					<?php
+						if(isset($_POST['enviar']))
 {
-echo $enviado;
-}
-?>
+echo $mensaje;
+}	
+	?>					
+					</div>
+
 					<div class="text-center p-t-12">
 						<span class="txt1">
 							
 						</span>
 						<a class="txt2" href="index.php">
-							Deseo logearme ?
+							Deseo logearme?
 						</a>
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="ingresecode.php">
-							Ya tengo mi codigo :) -->
+						<a class="txt2" href="registro.php">
+							Crear una cuenta de acceso
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
